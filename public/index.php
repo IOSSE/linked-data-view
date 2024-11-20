a323351a66992435a096c0a86e953113fbef3578
Thomas Riechert <thomas.riechert@htwk-leipzig.de>
2024-11-20 16:18:27 +0100

    check mod_rewrite

--- public/index.php
+++ public/index.php
@@ -1,4 +1,17 @@
 <?php
-// Query-Parameter entfernen
+// Check mod_rewrite
+if (function_exists('apache_get_modules')) {
+    $modules = apache_get_modules();
+    if (in_array('mod_rewrite', $modules)) {
+        echo "mod_rewrite ist aktiviert!";
+    } else {
+        echo "mod_rewrite ist NICHT aktiviert!"; exit;
+    }
+} else {
+    echo "Funktion apache_get_modules() nicht verfügbar."; exit;
+}
+
+// Query-Parameter-Path laden
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 echo "Angefragter Pfad: " . $path;
+
