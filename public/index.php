243ccaaa2a9e3c8a145f41f48a5b4e2050bb5586
Thomas Riechert <thomas.riechert@htwk-leipzig.de>
2024-11-20 16:36:07 +0100

    parameter

--- public/index.php
+++ public/index.php
@@ -2,10 +2,8 @@
 // Check mod_rewrite
 if (function_exists('apache_get_modules')) {
     $modules = apache_get_modules();
-    if (in_array('mod_rewrite', $modules)) {
-        echo "mod_rewrite ist aktiviert!";
-    } else {
-        echo "mod_rewrite ist NICHT aktiviert!"; exit;
+    if (!in_array('mod_rewrite', $modules)) {
+	echo "mod_rewrite ist NICHT aktiviert!"; exit;
     }
 } else {
     echo "Funktion apache_get_modules() nicht verfügbar."; exit;
@@ -13,5 +11,6 @@ if (function_exists('apache_get_modules')) {
 
 // Query-Parameter-Path laden
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-echo "Angefragter Pfad: " . $path;
+echo "\nAngefragte Url: " . $_SERVER['REQUEST_URI'];
+echo "\nAngefragter Pfad: " . $path;
 
